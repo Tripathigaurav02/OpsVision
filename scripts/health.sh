170ad85f86330911a503e839e64bce8714487292
Gaurav Tripathi <bussinessforgaurav@gmail.com>
2026-06-10 07:07:36 +0000

    now we have updated the script to store values in metrics file and not print the result on console

--- scripts/health.sh
+++ scripts/health.sh
@@ -11,17 +11,19 @@ DISK_USAGE=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%'
 DOCKER_STATUS=$(systemctl is-active docker)
 NGINX_STATUS=$(systemctl is-active nginx)
 
-echo "Hostname: $HOSTNAME"  
-echo "Current Time: $CURRENT_TIME"
-echo "Uptime: $UPTIME"
-echo "Memory Usage: ${MEMORY_USAGE}%"
-echo "Disk Usage: ${DISK_USAGE}%"
-echo "Docker Status: $DOCKER_STATUS"
-echo "Nginx Status: $NGINX_STATUS"
+#echo "Hostname: $HOSTNAME"  
+#echo "Current Time: $CURRENT_TIME"
+#echo "Uptime: $UPTIME"
+#echo "Memory Usage: ${MEMORY_USAGE}%"
+#echo "Disk Usage: ${DISK_USAGE}%"
+#echo "Docker Status: $DOCKER_STATUS"
+#echo "Nginx Status: $NGINX_STATUS"
 
 cat <<EOF > ../website/data/metrics.json
 {
   "hostname": "$HOSTNAME",
+  "Current Time": "$CURRENT_TIME",
+  "Uptime": "$UPTIME",
   "memory": $MEMORY_USAGE,
   "disk": $DISK_USAGE,
   "docker": "$DOCKER_STATUS",
